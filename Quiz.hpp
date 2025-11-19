a409379ab6c251c20b9ccb84c5e1e38add862639
sohaibm05 <thelearnee@gmail.com>
2025-11-19 12:33:56 +0500

    Update Quiz.hpp

--- Quiz.hpp
+++ Quiz.hpp
@@ -31,14 +31,54 @@ class Question
         int correctAnswer;
         int marks; // this means number of marks the question is worth
     public:
-        Question(int id, string text, int correctIndex, int m);
-        // int getQuestionID() const;
-        // string getQuestionText() const;
-        // int getMarks() const;
-        // int getCorrectAsnwer() const;
+        // Question(int id, string text, int correctIndex, int m); // i dont think i need this 
         virtual void displayQuestion() const = 0; // pure virtual function
         virtual bool checkAnswer(int answer) const = 0; // pure virtual function
         virtual ~Question() {}
 };
+
+
+class MCQ: public Question
+{
+    private:
+        vector<string> options;
+    public:
+        MCQ(int id, string text, vector<string> opts, int correctIndex, int m);
+        void displayQuestion() const override;
+        bool checkAnswer(int answer) const override;
+        void addOption(const string& option);
+};
+
+class TrueFalse: public Question
+{
+    private:
+        bool correctAnswer;
+    public:
+        TrueFalse(int id, string text, int correctIndex, int m);
+        void displayQuestion() const;
+        bool checkAnswer(int answer) const;
+};
+
+class fillInTheBlank: public Question
+{
+    private:
+        string correctAnswer;
+    public:
+        fillInTheBlank(int id, string text, string correctAns, int m);
+        void displayQuestion() const;
+        bool checkAnswer(int answer) const;
+};
+
+class Numerical_Question: public Question
+{
+    private:
+        double correctAnswer;
+    public:
+        Numerical_Question(int id, string text, double correctAns, int m);
+        void displayQuestion() const;
+        bool checkAnswer(int answer) const;
+};
+
+
 
 #endif
