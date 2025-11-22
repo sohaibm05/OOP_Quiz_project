5b4b5f006150486858ef1f7889d01921ba23c66b
sohaibm05 <thelearnee@gmail.com>
2025-11-22 11:59:41 +0500

    Fill in the blank push

---
+++ Project_folder/FillInTheBlank.hpp
@@ -0,0 +1,20 @@
+#ifndef FILLINTHEBLANK_HPP
+#define FILLINTHEBLANK_HPP
+
+#include "Question.hpp"
+#include <string>
+
+class FillInTheBlank : public Question 
+{
+private:
+    std::string correctAnswer;
+
+public:
+    FillInTheBlank(std::string id, std::string text, float marks); // Constructor
+
+    void setCorrectAnswer(std::string answer); //   Set the correct answer
+    bool checkAnswer(std::string answer) const; // Check if the provided answer is correct
+    void displayQuestion() const; // Display the question text
+};
+
+#endif // FILLINTHEBLANK_HPP
