30eca02b52551cd1de67f7dd658db982ea4e970d
sohaibm05 <thelearnee@gmail.com>
2025-11-24 08:41:21 +0500

    Update NumericalQuestion.cpp

--- Project_folder/NumericalQuestion.cpp
+++ Project_folder/NumericalQuestion.cpp
@@ -1,5 +1,6 @@
 #include "NumericalQuestion.hpp"
 #include <iostream>
+using namespace std;
 
 NumericalQuestion::NumericalQuestion(string id, string text, float marks): Question(id, text, marks), correctAnswer(0.0), tolerance(0.0) {}
 
@@ -9,18 +10,21 @@ void NumericalQuestion::setCorrectAnswer(double answer, double tol)
     tolerance = tol;
 }
 
-bool NumericalQuestion::checkAnswer(string answer) const 
+bool NumericalQuestion::checkAnswer(string answer) const // override base virtual
 {
-    try {
-        double val = std::stod(answer);
-        double diff = std::abs(val - correctAnswer);
-        if (tolerance > 0.0 && tolerance < 1.0) {
-            return diff <= std::abs(correctAnswer) * tolerance;
+    try 
+    {
+        double val = stod(answer);
+        double diff = abs(val - correctAnswer); // absolute difference
+        if (tolerance > 0.0 && tolerance < 1.0) 
+        {
+            return diff <= abs(correctAnswer) * tolerance; //  relative tolerance
         }
         return diff <= tolerance;
-    } catch (...) {
-        return false;
-    }
+    } catch (...) {return false;}
 }
 
-void NumericalQuestion::displayQuestion() const { std::cout << questionText << std::endl; }
+void NumericalQuestion::displayQuestion() const // override base virtual
+{ 
+    cout << questionText << endl; 
+}
