52a38db70e84a5806282e5aee7a06d05f5e5c59a
sohaibm05 <thelearnee@gmail.com>
2025-11-26 09:12:29 +0500

    Update CreateQuestion.hpp

--- Project_folder/CreateQuestion.hpp
+++ Project_folder/CreateQuestion.hpp
@@ -9,12 +9,7 @@
 
 namespace CreateQuestion 
 {
-    // Prompts the user to create a single question interactively.
-    // Returns a newly allocated Question* (caller owns it) or nullptr if the user chose to finish/cancel.
-    // Ownership: the returned Question* is newly allocated and ownership is
-    // transferred to the caller (usually added to a Quiz via addQuestion).
-    // Caller must delete it if not added to a Quiz.
-    Question* createQuestionInteractive();
+    Question* createQuestionInteractive(); // made this just so that main function is smaler and cleaner
 }
 
 #endif // CREATEQUESTION_HPP
